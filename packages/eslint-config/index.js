35db44b5f8257997f404c7f401738cfb747272aa
Enis Jetishi <enis.jetishi@outlook.com>
2025-12-18 13:34:57 +0100

    chore: monorepo setup

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: false,
+  env: {
+    browser: true,
+    node: true,
+    es2022: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+};
